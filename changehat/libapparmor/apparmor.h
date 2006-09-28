5b3efd982fb8b37f3c75aa4a6b8d4897971e4f5e
Steve Beattie <steve@nxnw.org>
2006-09-28 06:45:55 +0000

    Unfortunately, the forge website(s) don't provide information on how to use the change_hat(2) function; point them at the manpage for usage information.

--- changehat/libapparmor/apparmor.h
+++ changehat/libapparmor/apparmor.h
@@ -13,7 +13,9 @@
 __BEGIN_DECLS
 
 /* Prototype for change_hat as defined by the AppArmor project
- * <http://forge.novell.com/modules/xfmod/project/?apparmor> */
+   <http://forge.novell.com/modules/xfmod/project/?apparmor>
+   Please see the change_hat(2) manpage for information. */
+
 extern int change_hat(const char *subprofile, unsigned int magic_token);
 
 __END_DECLS
