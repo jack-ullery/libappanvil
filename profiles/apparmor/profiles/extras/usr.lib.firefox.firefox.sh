7d8f597c86c72b5e6152750a4c1c01ac538325db
John Johansen <john.johansen@canonical.com>
2008-11-05 16:00:39 +0000

    Update firefox profile as base for firefox 3

--- profiles/apparmor/profiles/extras/usr.lib.firefox.firefox.sh
+++ profiles/apparmor/profiles/extras/usr.lib.firefox.firefox.sh
@@ -1,35 +1,19 @@
-# vim:syntax=apparmor
-# Last Modified: Fri Feb 17 17:45:24 2006
-# ------------------------------------------------------------------
-#
-#    Copyright (C) 2002-2005 Novell/SUSE
-#
-#    This program is free software; you can redistribute it and/or
-#    modify it under the terms of version 2 of the GNU General Public
-#    License published by the Free Software Foundation.
-#
-# ------------------------------------------------------------------
-
+# Last Modified: Wed Nov  5 03:32:59 2008
 #include <tunables/global>
 
 /usr/lib/firefox/firefox.sh {
   #include <abstractions/base>
   #include <abstractions/bash>
   #include <abstractions/consoles>
-  #include <abstractions/user-tmp>
 
-  /bin/basename mixr,
-  /bin/bash mixr,
-  /bin/gawk mixr,
-  /bin/grep mixr,
+  deny capability sys_ptrace,
+
+  /bin/basename rix,
+  /bin/bash rix,
+  /bin/grep rix,
   /etc/magic r,
-  /usr/bin/file mixr,
-  /usr/bin/which mixr,
-  /usr/lib/firefox/* r,
-# the shell script relies on LD_PRELOAD.
-  /usr/lib/firefox/firefox-bin px,
-  /usr/lib/firefox/firefox.sh rmix,
-# the shell script relies on LD_PRELOAD.
-  /usr/lib/firefox/mozilla-xremote-client px,
+  /usr/bin/file rix,
+  /usr/lib/firefox/firefox px,
   /usr/share/misc/magic.mgc r,
+
 }
