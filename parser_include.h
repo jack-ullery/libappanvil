b69ad7f7f7dbda66cedbb4cb8317384df57c1f45
Kees Cook <kees.cook@canonical.com>
2010-06-04 18:57:01 -0700

    hrm, since I added code, I need to update the copyright details.

--- parser_include.h
+++ parser_include.h
@@ -1,6 +1,8 @@
 /*
  *   Copyright (c) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007
  *   NOVELL (All rights reserved)
+ *   Copyright (c) 2010
+ *   Canonical, Ltd.
  *
  *   This program is free software; you can redistribute it and/or
  *   modify it under the terms of version 2 of the GNU General Public
@@ -12,7 +14,7 @@
  *   GNU General Public License for more details.
  *
  *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, contact Novell, Inc.
+ *   along with this program; if not, contact Canonical, Ltd.
  */
 
 #ifndef PARSER_INCLUDE_H
