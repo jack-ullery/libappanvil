b5cd93aa2f1e4357b6b46fe5493809f523292147
Steve Beattie <sbeattie@ubuntu.com>
2011-02-08 14:18:39 -0800

    Purge deprecated gnome apparmor applet, aa-notify is the preferred notification mechanism nowadays.
    Acked-by: John Johansen <john.johansen@canonical.com>

--- deprecated/management/applets/apparmorapplet-gnome/src/preferences_dialog.h
+++
@@ -1,9 +0,0 @@
-#ifndef PREFERENCES_DIALOG_H
-#define PREFERENCES_DIALOG_H
-
-GtkWidget* create_preferences_dialog (void);
-void button_press (GtkDialog * dialog, gint answer, gpointer data);
-
-#endif
-
-
