3dde3d5322f39bf08058d5fb600a4aa2a41a5d25
Steve Beattie <sbeattie@ubuntu.com>
2011-02-23 14:02:45 -0800

    libraries/libapparmor/: more license cleanups, adjust my email address in the AUTHORS file.

--- libraries/libapparmor/src/apparmor.h
+++ libraries/libapparmor/src/apparmor.h
@@ -6,6 +6,11 @@
  * Lesser General Public License, version 2.1. Please see the file
  * COPYING.LGPL.
  *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
--- libraries/libapparmor/src/libimmunix_warning.c
+++ libraries/libapparmor/src/libimmunix_warning.c
@@ -1,10 +1,18 @@
 /*
-     Copyright (c) 2006 Novell, Inc. (All rights reserved)
-     The libimmunix library is licensed under the terms of the GNU
-     Lesser General Public License, version 2.1. Please see the file
-     COPYING.LGPL.
-
-*/
+ * Copyright (c) 2006 Novell, Inc. (All rights reserved)
+ *
+ * The libimmunix library is licensed under the terms of the GNU
+ * Lesser General Public License, version 2.1. Please see the file
+ * COPYING.LGPL.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <syslog.h>
 
