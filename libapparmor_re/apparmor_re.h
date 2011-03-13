477777e55ab9490a27d3955e82319bd13847dc48
John Johansen <john.johansen@canonical.com>
2011-03-13 05:49:15 -0700

    Split out aare_rules which are used to encapsulate creating the dfa
    Split out the aare_rule bits that encapsulate the convertion of apparmor
    rules into the final compressed dfa.
    
    This patch will not compile because of the it needs hfa to export an interface
    but hfa is going to be split so just delay until hfa and transtable are
    split and they can each export their own interface.
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Acked-By: Steve Beattie <sbeattie@ubuntu.com>

--- libapparmor_re/apparmor_re.h
+++ libapparmor_re/apparmor_re.h
@@ -43,25 +43,4 @@ typedef enum dfaflags {
   DFA_DUMP_NODE_TO_DFA =	1 << 31,
 } dfaflags_t;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-struct aare_ruleset;
-
-typedef struct aare_ruleset aare_ruleset_t;
-
-aare_ruleset_t *aare_new_ruleset(int reverse);
-void aare_delete_ruleset(aare_ruleset_t *rules);
-int aare_add_rule(aare_ruleset_t *rules, char *rule, int deny,
-		  uint32_t perms, uint32_t audit, dfaflags_t flags);
-int aare_add_rule_vec(aare_ruleset_t *rules, int deny, uint32_t perms,
-		      uint32_t audit, int count, char **rulev, dfaflags_t flags);
-void *aare_create_dfa(aare_ruleset_t *rules, size_t *size, dfaflags_t flags);
-void aare_reset_matchflags(void);
-
-#ifdef __cplusplus
-}
-#endif
-
 #endif /* APPARMOR_RE_H */
