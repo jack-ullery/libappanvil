ca5cd28d48f225618d1ea9926768e11548d94e74
Christian Boltz <apparmor@cboltz.de>
2011-08-19 00:24:48 +0200

    add syslog capability to apparmor.vim
    Acked-by: John Johansen <john.johansen@canonical.com>

--- utils/vim/create-apparmor.vim.sh
+++ utils/vim/create-apparmor.vim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # not-too-dangerous capabilities
-sdKapKey="chown dac_override dac_read_search fowner fsetid kill setgid setuid setpcap linux_immutable net_bind_service net_broadcast net_admin net_raw ipc_lock ipc_owner sys_chroot sys_ptrace sys_pacct sys_boot sys_nice sys_resource sys_time sys_tty_config mknod lease"
+sdKapKey="chown dac_override dac_read_search fowner fsetid kill setgid setuid setpcap linux_immutable net_bind_service net_broadcast net_admin net_raw ipc_lock ipc_owner sys_chroot sys_ptrace sys_pacct sys_boot sys_nice sys_resource sys_time sys_tty_config syslog mknod lease"
 
 # dangerous capabilities
 sdKapKeyDanger="audit_control audit_write mac_override mac_admin set_fcap sys_admin sys_module sys_rawio"
