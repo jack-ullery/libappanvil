e61b7b9241c61191dcd16c83dbe0cce4c72ad5a9
John Johansen <john.johansen@canonical.com>
2012-02-24 04:21:59 -0800

    Update the copyright dates for the apparmor_parser
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- parser/parser_main.c
+++ parser/parser_main.c
@@ -2,8 +2,8 @@
  *   Copyright (c) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007
  *   NOVELL (All rights reserved)
  *
- *   Copyright (c) 2010, 2011
- *   Canonical, Ltd. (All rights reserved)
+ *   Copyright (c) 2010 - 2012
+ *   Canonical Ltd. (All rights reserved)
  *
  *   This program is free software; you can redistribute it and/or
  *   modify it under the terms of version 2 of the GNU General Public
@@ -60,7 +60,7 @@
 #define UNPRIVILEGED_OPS (!(PRIVILEGED_OPS))
 
 const char *parser_title	= "AppArmor parser";
-const char *parser_copyright	= "Copyright (C) 1999-2008 Novell Inc.\nCopyright 2009-2011 Canonical Ltd.";
+const char *parser_copyright	= "Copyright (C) 1999-2008 Novell Inc.\nCopyright 2009-2012 Canonical Ltd.";
 
 char *progname;
 int opt_force_complain = 0;
--- parser/parser_policy.c
+++ parser/parser_policy.c
@@ -2,8 +2,8 @@
  *   Copyright (c) 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007
  *   NOVELL (All rights reserved)
  *
- *   Copyright (c) 2010
- *   Canonical, Ltd. (All rights reserved)
+ *   Copyright (c) 2010 - 2012
+ *   Canonical Ltd. (All rights reserved)
  *
  *   This program is free software; you can redistribute it and/or
  *   modify it under the terms of version 2 of the GNU General Public
