1fdc3a5e999fac1f96af00a899c1c9cfd653d0bc
Jamie Strandboge <jamie@ubuntu.com>
2012-08-23 19:36:25 -0500

    utils/apparmor/sandbox.py: - add --xserver option and support both xephyr and xpra - refactoring

--- utils/apparmor/sandbox.py
+++ utils/apparmor/sandbox.py
@@ -44,6 +44,10 @@ def parse_args(args=None, parser=None):
                       default=False,
                       help='Run in isolated X server',
                       action='store_true')
+    parser.add_option('--xserver',
+                      dest='xserver',
+                      default='xpra',
+                      help='Nested X server to use (default is xpra)')
     parser.add_option('-d', '--debug',
                       dest='debug',
                       default=False,
@@ -59,6 +63,9 @@ def parse_args(args=None, parser=None):
         apparmor.common.DEBUGGING = True
     if my_opt.template == "default":
         if my_opt.withx:
+            if my_opt.xserver.lower() != 'xpra' and \
+               my_opt.xserver.lower() != 'xephyr':
+                error("Invalid server '%s'. Use 'xpra' or 'xephyr'")
             my_opt.template = "sandbox-x"
         else:
             my_opt.template = "sandbox"
@@ -103,25 +110,6 @@ def aa_exec(command, opt):
     rc, report = cmd(args)
     return rc, report
 
-def find_free_x_display():
-    '''Find a free X display'''
-    x_display = ""
-    current = os.environ["DISPLAY"]
-    for i in range(1,257): # TODO: this puts an artificial limit of 256
-                           #       sandboxed applications
-        tmp = ":%d" % i
-        os.environ["DISPLAY"] = tmp
-        rc, report = cmd(['xset', '-q'])
-        if rc != 0:
-            x_display = tmp
-            break
-        
-    os.environ["DISPLAY"] = current
-    if x_display == "":
-        raise AppArmorException("Could not find available X display")
-
-    return x_display
-
 def run_sandbox(command, opt):
     '''Run application'''
     # aa-exec
@@ -129,68 +117,156 @@ def run_sandbox(command, opt):
     rc, report = aa_exec(command, opt)
     return rc, report
 
+class SandboxXserver():
+    def __init__(self, resolution, title):
+        self.resolution = resolution
+        self.title = title
+        self.pids = []
+        self.find_free_x_display()
+
+    def find_free_x_display(self):
+        '''Find a free X display'''
+        display = ""
+        current = os.environ["DISPLAY"]
+        for i in range(1,257): # TODO: this puts an artificial limit of 256
+                               #       sandboxed applications
+            tmp = ":%d" % i
+            os.environ["DISPLAY"] = tmp
+            rc, report = cmd(['xset', '-q'])
+            if rc != 0:
+                display = tmp
+                break
+        
+        os.environ["DISPLAY"] = current
+        if display == "":
+            raise AppArmorException("Could not find available X display")
+
+        self.display = display
+
+    def cleanup(self):
+        '''Cleanup our forked pids, etc'''
+        # kill server now. It should've terminated, but be sure
+        for pid in self.pids:
+            cmd(['kill', '-15', "%d" % pid])
+            os.kill(pid, 15)
+            os.waitpid(pid, 0)
+
+    def start(self):
+        '''start() should be overridden'''
+
+class SandboxXephyr(SandboxXserver):
+    def start(self):
+        '''Start a Xephyr server'''
+        listener_x = os.fork()
+        if listener_x == 0:
+            # TODO: break into config file? Which are needed?
+            x_exts = ['-extension', 'GLX',
+                      '-extension', 'MIT-SHM',
+                      '-extension', 'RENDER',
+                      '-extension', 'SECURITY',
+                      '-extension', 'DAMAGE'
+                     ]
+            # verify_these
+            x_extra_args = ['-host-cursor', # less secure?
+                            '-fakexa',      # for games? seems not needed
+                            '-nodri',       # more secure?
+                           ]
+
+            x_args = ['-nolisten', 'tcp',
+                      '-screen', self.resolution,
+                      '-br',        # black background
+                      '-reset',     # reset after last client exists
+                      '-terminate', # terminate at server reset
+                      '-title', self.title,
+                      ] + x_exts + x_extra_args
+
+            args = ['/usr/bin/Xephyr'] + x_args + [self.display]
+            debug(" ".join(args))
+            sys.stderr.flush()
+            os.execv(args[0], args)
+            sys.exit(0)
+        self.pids.append(listener_x)
+
+        time.sleep(0.2) # FIXME: detect if running
+
+        # Next, start the window manager
+        sys.stdout.flush()
+        os.chdir(os.environ["HOME"])
+        listener_wm = os.fork()
+        if listener_wm == 0:
+            # update environment
+            os.environ["DISPLAY"] = self.display
+            debug("DISPLAY is now '%s'" % os.environ["DISPLAY"])
+
+            args = ['/usr/bin/matchbox-window-manager', '-use_titlebar', 'no']
+            debug(" ".join(args))
+            sys.stderr.flush()
+            os.execv(args[0], args)
+            sys.exit(0)
+
+        self.pids.append(listener_wm)
+        time.sleep(0.2) # FIXME: detect if running
+
+class SandboxXpra(SandboxXserver):
+    def cleanup(self):
+        cmd(['xpra', 'stop', self.display])
+        SandboxXserver.cleanup(self)
+        
+    def start(self):
+        listener_x = os.fork()
+        if listener_x == 0:
+            x_args = ['--no-daemon',
+                      '--no-clipboard',
+                      '--no-pulseaudio']
+            # TODO: --password-file
+            args = ['/usr/bin/xpra', 'start', self.display] + x_args
+            debug(" ".join(args))
+            sys.stderr.flush()
+            os.execv(args[0], args)
+            sys.exit(0)
+        self.pids.append(listener_x)
+
+        time.sleep(2) # FIXME: detect if running
+
+        # Next, attach to xpra
+        sys.stdout.flush()
+        os.chdir(os.environ["HOME"])
+        listener_attach = os.fork()
+        if listener_attach == 0:
+            # TODO: --pasword-file
+            args = ['/usr/bin/xpra', 'attach', self.display,
+                                 '--title=%s' % self.title]
+            debug(" ".join(args))
+            sys.stderr.flush()
+            os.execv(args[0], args)
+            sys.exit(0)
+
+        self.pids.append(listener_attach)
+
 def run_xsandbox(command, opt):
     '''Run X application in a sandbox'''
-    # Find a display to run on
-    x_display = find_free_x_display()
-
-    debug ("DISPLAY=%s" % os.environ["DISPLAY"])
-
-    # first, start X
-    listener_x = os.fork()
-    if listener_x == 0:
-        # TODO: break into config file? Which are needed?
-        x_exts = ['-extension', 'GLX',
-                  '-extension', 'MIT-SHM',
-                  '-extension', 'RENDER',
-                  '-extension', 'SECURITY',
-                  '-extension', 'DAMAGE'
-                 ]
-        # verify_these
-        x_extra_args = ['-host-cursor', # less secure?
-                        '-fakexa',      # for games? seems not needed
-                        '-nodri',       # more secure?
-                       ]
-
-        x_args = ['-nolisten', 'tcp',
-                  '-screen', opt.resolution,
-                  '-br',        # black background
-                  '-reset',     # reset after last client exists
-                  '-terminate', # terminate at server reset
-                  '-title', command[0],
-                  ] + x_exts + x_extra_args
-
-        args = ['/usr/bin/Xephyr'] + x_args + [x_display]
-        debug(" ".join(args))
-        sys.stderr.flush()
-        os.execv(args[0], args)
-        sys.exit(0)
-
     # save environment
     old_display = os.environ["DISPLAY"]
+    debug ("DISPLAY=%s" % old_display)
     old_cwd = os.getcwd()
 
+    # first, start X
+    if opt.xserver.lower() == "xephyr":
+        x = SandboxXephyr(opt.resolution, command[0])
+    else:
+        x = SandboxXpra(opt.resolution, command[0])
+    x.start()
+
     # update environment
-    os.environ["DISPLAY"] = x_display
+    os.environ["DISPLAY"] = x.display
     debug("DISPLAY is now '%s'" % os.environ["DISPLAY"])
 
-    time.sleep(0.2) # FIXME: detect if running
-
-    # Next, start the window manager
-    sys.stdout.flush()
-    os.chdir(os.environ["HOME"])
-    listener_wm = os.fork()
-    if listener_wm == 0:
-        args = ['/usr/bin/matchbox-window-manager', '-use_titlebar', 'no']
-        debug(" ".join(args))
-        sys.stderr.flush()
-        os.execv(args[0], args)
-        sys.exit(0)
-
-    time.sleep(0.2) # FIXME: detect if running
-
     # aa-exec
-    rc, report = aa_exec(command, opt)
+    try:
+        rc, report = aa_exec(command, opt)
+    except:
+        x.cleanup()
+        raise
 
     # reset environment
     os.environ["DISPLAY"] = old_display
@@ -198,12 +274,6 @@ def run_xsandbox(command, opt):
 
     os.chdir(old_cwd)
 
-    # kill server now. It should've terminated, but be sure
-    cmd(['kill', '-15', "%d" % listener_wm])
-    os.kill(listener_wm, 15)
-    os.waitpid(listener_wm, 0)
-    cmd(['kill', '-15', "%d" % listener_x])
-    os.kill(listener_x, 15)
-    os.waitpid(listener_x, 0)
+    x.cleanup()
 
     return rc, report
