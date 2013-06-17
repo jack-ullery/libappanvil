758d1c6e7da8e1993638a8944a1dbe46a33c9a84
Kshitij Gupta <kgupta8592@gmail.com>
2013-06-18 03:49:05 +0530

    added severity.py with tested convert_regex and the old and new config

--- lib/config.py
+++ lib/config.py
@@ -1,42 +1,16 @@
 import os
-import re
 import stat
+import configparser
 
 confdir = '/etc/apparmor'
 cfg = None
 repo_cfg = None
 
 def read_config(filename):
-    """Reads the file and returns a double dictionary config[section][attribute]=property"""
-    config = dict()
-    regex_label = re.compile('^\[(\S+)\]')
-    regex_value = re.compile('^\s*(\S+)\s*=\s*(.*)\s*$')
+    """Reads the file and returns a configparser config[section][attribute]=property"""
+    config = configparser.ConfigParser()
     filepath = confdir + '/' + filename
-    try:
-        conf_file = open(filepath, 'r', 1)
-    except IOError:
-        pass
-    else:
-        section = ''  # The default section
-        for line in conf_file:
-            # Ignore the comment lines
-            if line.lstrip().startswith('#'):       
-                continue
-            line = line.rstrip('\n')
-            # Search for a new section
-            label_match = regex_label.search(line)     
-            if label_match:
-                section = label_match.groups()[0]
-            else:
-                # Search for a attribute value pair
-                value_match = regex_value.search(line)        
-                if value_match:
-                    attribute = value_match.groups()[0]
-                    value = value_match.groups()[1]
-                    # A doubly nested dictionary
-                    config[section] = config.get(section, {})
-                    config[section][attribute] = value
-        conf_file.close()
+    config.read(filepath)
     # LP: Bug #692406
     # Explicitly disabled repository
     if filename == "repository.conf":
@@ -44,25 +18,17 @@ def read_config(filename):
     return config
         
 def write_config(filename, config):
-    """Writes the given configuration to the specified file"""
+    """Writes the given configparser to the specified file"""
     filepath = confdir + '/' + filename
     try:
-        conf_file = open(filepath, 'w') 
+        with open(filepath, 'w') as config_file:
+            config.write(config_file) 
     except IOError:
         raise IOError("Unable to write to %s"%filename)
     else:
-        for section in sorted(config.keys()):
-            # Write the section and all attributes and values under the section
-            if section != '':     # If default section then no section label
-                conf_file.write("[%s]\n"%section)
-            for attribute in sorted(config[section].keys()):
-                if section != '':
-                    conf_file.write("  ")   # Indentation for a section
-                conf_file.write("%s = %s\n"%(attribute, config[section][attribute]))
         permission_600 = stat.S_IRUSR | stat.S_IWUSR    # Owner read and write
         # Set file permissions as 0600
         os.chmod(filepath, permission_600)
-        conf_file.close()
 
 def find_first_file(file_list):
     """Returns name of first matching file None otherwise"""
---
+++ lib/severity.py
@@ -0,0 +1,67 @@
+import os
+import re
+
+class Severity:
+    def __init__(self, dbname=None, default_rank=10):
+        self.severity = dict()
+        self.severity['DATABASENAME'] = dbname
+        self.severity['CAPABILITIES'] = {}
+        self.severity['FILES'] = {}
+        self.severity['REGEXPS'] = {}
+        self.severity['DEFAULT_RANK'] = default_rank
+        if not dbname:
+            return self.severity
+        try:
+            database = open(dbname, 'r')
+        except IOError:
+            raise("Could not open severity database %s"%dbname)
+        for line in database:
+            line = line.strip() # or only rstrip and lstrip?
+            if line == '' or line.startswith('#') :
+                continue
+            if line.startswith('/'):
+                try:
+                    path, read, write, execute = line.split()
+                except ValueError:
+                    raise("Insufficient values for permissions")
+                else:
+                    path = path.lstrip('/')
+                    if '*' not in path:
+                        self.severity['FILES'][path] = {'r': read, 'w': write, 'x': execute}
+                    else:
+                        ptr = self.severity['REGEXPS']
+                        pieces = path.split('/')
+                        for index, piece in enumerate(pieces):
+                            if '*' in piece:
+                                path = '/'.join(pieces[index:])
+                                regexp = self.convert_regexp(path)
+                                ptr[regexp] = {'SD_RANK': {'r': read, 'w': write, 'x': execute}}
+                                break
+                            else:
+                                ptr[piece] = ptr.get(piece, {})
+                                ptr = ptr[piece]
+            elif line.startswith('CAP'):
+                resource, severity = line.split()
+                self.severity['CAPABILITIES'][resource] = severity
+            else:
+                print("unexpected database line: %s"%line)   
+        database.close()
+        
+    def convert_regexp(self, path):
+        pattern_or = re.compile('{.*|,.*}')    # The regex pattern for {a,b}
+        regex = path
+        for character in ['.', '+', '[', ']']:    # Escape the regex symbols
+            regex = regex.replace(character, "\%s"%character)
+        # Convert the ** to regex
+        regex = regex.replace('**', '.SDPROF_INTERNAL_GLOB')
+        # Convert the * to regex
+        regex = regex.replace('*', '[^/]SDPROF_INTERNAL_GLOB')
+        # Convert {a,b} to (a|b) form
+        if pattern_or.match(regex):
+            for character, replacement in zip('{},', '()|'):
+                regex = regex.replace(character, replacement)
+        # Restore the * in the final regex
+        regex = regex.replace('SDPROF_INTERNAL_GLOB', '*')
+        return regex
+    
+    
