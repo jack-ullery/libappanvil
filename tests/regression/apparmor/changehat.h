71b441020e94d5bd1d2f740a179187a736957168
Tyler Hicks <tyhicks@canonical.com>
2013-11-04 12:25:53 -0800

    tests: Remove remainder of libimmunix and 2.[24] kernel support
    The previous patch for removing libimmunix support from the regression
    tests wasn't complete. Also, the 2.2 and 2.4 kernel support code is
    closely related and can be removed considering how old those kernels
    are.
    
    Signed-off-by: Tyler Hicks <tyhicks@canonical.com>
    Acked-by: Steve Beattie <steve@nxnw.org>

--- tests/regression/apparmor/changehat.h
+++ tests/regression/apparmor/changehat.h
@@ -1,51 +1,8 @@
-
-/* #define CHANGEHAT_NOT_IN_GLIB */
-
-#define SD_ID_MAGIC     0x8c235e38
-
-#ifdef CHANGEHAT_NOT_IN_LIBRARY
-#  ifdef CHANGEHAT_2_4_KERNEL
-
-struct sd_hat {
-        char *hat_name;
-        unsigned int hat_magic;
-};
-
-#define __NR_security   223 
-#define SD_CHANGE_HAT   10
-
-_syscall3(int, security, unsigned int, id, unsigned int, call, unsigned long *, args);
-
-int change_hat (char * subprofile, unsigned int token)
-{
-	struct sd_hat hat;
-	unsigned int id = SD_ID_MAGIC;
-
-	hat.hat_name=subprofile;
-	hat.hat_magic = token;
-
-	return security(id, SD_CHANGE_HAT, (unsigned long*)&hat);
-}
-
-#  else
-#    ifdef CHANGEHAT_2_2_KERNEL
-#define __NR_change_hat 230
-
-_syscall2(int, change_hat, char *, subdomain, unsigned int, magic_token);
-
-#    endif /* CHANGEHAT_2_2_KERNEL */
-#  endif /* CHANGEHAT_2_4_KERNEL */
-
-#else /* !CHANGEHAT_NOT_IN_LIBRARY */
-#ifdef USE_COMPAT_IMMUNIX_H
-#include <sys/immunix.h>
-#else
-#include <sys/apparmor.h>
-#endif /* USE_COMPAT_IMMUNIX_H */
-#endif /* CHANGEHAT_NOT_IN_LIBRARY */
-
 #include <fcntl.h>
 #include <string.h>
+#include <sys/apparmor.h>
+
+#define SD_ID_MAGIC     0x8c235e38
 
 inline int do_open (char * file)
 {
