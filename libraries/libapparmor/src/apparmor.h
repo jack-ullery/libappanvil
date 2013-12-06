500c5cca68ee1cb967d11fed0e5c2a4569125a0e
Tyler Hicks <tyhicks@canonical.com>
2013-12-06 11:20:40 -0800

    Remove private mediation class types from apparmor.h
    Currently, D-Bus rules are the only type of policy that we expect to be
    queried from userspace. Therefore, we do not need to export other
    mediation types at this time.
    
    This patch removes all AA_CLASS_* macros, except AA_CLASS_DBUS, from
    libapparmor's apparmor.h header. These macros are already defined in the
    parser's policydb.h header.
    
    Signed-off-by: Tyler Hicks <tyhicks@canonical.com>
    Acked-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Seth Arnold <seth.arnold@canonical.com>

--- libraries/libapparmor/src/apparmor.h
+++ libraries/libapparmor/src/apparmor.h
@@ -24,23 +24,10 @@
 __BEGIN_DECLS
 
 /*
- * Class of mediation types in the AppArmor policy db
+ * Class of public mediation types in the AppArmor policy db
  */
-#define AA_CLASS_COND		0
-#define AA_CLASS_UNKNOWN	1
-#define AA_CLASS_FILE		2
-#define AA_CLASS_CAP		3
-#define AA_CLASS_NET		4
-#define AA_CLASS_RLIMITS	5
-#define AA_CLASS_DOMAIN		6
-#define AA_CLASS_MOUNT		7
-#define AA_CLASS_NS_DOMAIN	8
-#define AA_CLASS_PTRACE		9
-
-#define AA_CLASS_ENV		16
 
 #define AA_CLASS_DBUS		32
-#define AA_CLASS_X		33
 
 
 /* Permission flags for the AA_CLASS_DBUS mediation class */
