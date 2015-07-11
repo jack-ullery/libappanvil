9abcff03f4842b478a70265883c2dbe3375f5000
John Johansen <john.johansen@canonical.com>
2015-07-10 18:11:17 -0700

    Add support for variable expansion in profile names, and attachments
    allow
      @{FOO}=bar
      /foo@{FOO} { }
    
    to be expanded into
      /foobar { }
    
    and
      @{FOO}=bar baz
      /foo@{FOO} { }
    
    to be expanded into
      /foo{bar,baz} { }
    which is used as a regular expression for attachment purposes
    
    Further allow variable expansion in attachment specifications
      profile foo /foo@{FOO} { }
    
    profile name (if begun with profile keyword) and attachments to begin
    with a variable
      profile @{FOO} { }
      profile /foo @{FOO} { }
      profile @{FOO} @{BAR} {}
    
    hats
      ^@{FOO}
      hat @{FOO}
    
    and for subprofiles as well
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Steve Beattie <steve@nxnw.org>

--- parser_variable.c
+++ parser_variable.c
@@ -275,12 +275,29 @@ static int process_variables_in_rules(Profile &prof)
 	return 0;
 }
 
+static int process_variables_in_name(Profile &prof)
+{
+	/* this needs to be done before alias expansion, ie. altnames are
+	 * setup
+	 */
+	int error = expand_entry_variables(&prof.name);
+	if (!error && prof.attachment)
+		error = expand_entry_variables(&prof.attachment);
+
+	return error;
+}
 
 int process_profile_variables(Profile *prof)
 {
 	int error = 0, rc;
 
-	error = new_set_var(PROFILE_NAME_VARIABLE, prof->get_name(true).c_str());
+	/* needs to be before PROFILE_NAME_VARIABLE so that variable will
+	 * have the correct name
+	 */
+	error = process_variables_in_name(*prof);
+
+	if (!error)
+		error = new_set_var(PROFILE_NAME_VARIABLE, prof->get_name(true).c_str());
 
 	if (!error)
 		error = process_variables_in_entries(prof->entries);
