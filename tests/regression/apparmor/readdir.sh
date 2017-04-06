5452095203d018dfd8a66b3335b90daddceafb0d
Steve Beattie <sbeattie@ubuntu.com>
2017-04-05 21:34:24 -0700

    tests: readdir - test both getdents() and getdents64() if available
    In commit 3649, Colin King fixed the readdir test build issue where
    aarch64 only supports getdetns64(), not getdents(). Realistically,
    however, we want to ensure mediation occurs on both syscalls where
    they exist. This patch changes the test to attempt performing both
    versions of getdents(). Because we want to catch the situation where
    the result of getdents differs from getdents64, we now pass in the
    expected result.
    
    Also add a test to verify that having write access does not grant
    the ability to read a directory's contents.
    
    Bug: https://bugs.launchpad.net/bugs/1674245
    
    Signed-off-by: Steve Beattie <steve@nxnw.org>
    Acked-by: Seth Arnold <seth.arnold@canonical.com>
    Acked-by: Tyler Hicks <tyhicks@canonical.com>

--- tests/regression/apparmor/readdir.sh
+++ tests/regression/apparmor/readdir.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 #	Copyright (C) 2002-2005 Novell/SUSE
+#	Copyright (C) 2017 Canonical, Ltd.
 #
 #	This program is free software; you can redistribute it and/or
 #	modify it under the terms of the GNU General Public License as
@@ -8,7 +9,7 @@
 
 #=NAME readdir
 #=DESCRIPTION 
-# AppArmor requires 'r' permission on a directory in order for a confined task 
+# AppArmor requires 'r' permission on a directory in order for a confined task
 # to be able to read the directory contents.  This test verifies this.
 #=END
 
@@ -26,20 +27,31 @@ badperm=ix
 
 mkdir $dir
 
+# The readdir utility expects the return value to be passed as the
+# second argument and returns success if the succeeding or failing calls
+# match the expected value. It will fail the test if they don't, so for
+# example the result differs acrorss getdents() and getdents64() this
+# will be detected.
+
 # READDIR TEST
 genprofile $dir/:$okperm
-runchecktest "READDIR" pass $dir
+runchecktest "READDIR" pass $dir 0
 
+EACCES=13
 # READDIR TEST (no perm)
 genprofile $dir/:$badperm
-runchecktest "READDIR (no perm)" fail $dir
+runchecktest "READDIR (no perm)" pass $dir ${EACCES}
+
+# READDIR TEST (write perm) - ensure write perm isn't sufficient
+genprofile $dir/:w
+runchecktest "READDIR (write perm)" pass $dir ${EACCES}
 
 # this test is to make sure the raw 'file' rule allows access
 # to directories
 genprofile file
-runchecktest "READDIR 'file' dir" pass $dir
+runchecktest "READDIR 'file' dir" pass $dir 0
 
 # this test is to make sure the raw 'file' rule allows access
 # to '/'
 genprofile file
-runchecktest "READDIR 'file' '/'" pass '/'
+runchecktest "READDIR 'file' '/'" pass '/' 0
