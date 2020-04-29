801553588256e3e12707f5c7355cf75ed75cedeb
John Johansen <john.johansen@canonical.com>
2020-04-29 14:59:12 -0700

    binutils: make aa-enabled LSM stacking aware
    Make aa-enabled able to distinguish being enabled and enabled without
    access to the shared LSM iterfaces.
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- binutils/aa_enabled.c
+++ binutils/aa_enabled.c
@@ -20,6 +20,7 @@ void print_help(const char *command)
 {
 	printf(_("%s: [options]\n"
 		 "  options:\n"
+		 "  -x | --exclusive    Shared interfaces must be availabe\n"
 		 "  -q | --quiet        Don't print out any messages\n"
 		 "  -h | --help         Print help\n"),
 	       command);
@@ -50,8 +51,11 @@ static void exit_with_error(int saved_errno, int quiet)
 		if (!quiet)
 			printf(_("Maybe - insufficient permissions to determine availability.\n"));
 		exit(4);
+	case EBUSY:
+		if (!quiet)
+			printf(_("Partially - public shared interfaces are not available.\n"));
+		exit(10);
 	}
-
 	if (!quiet)
 		printf(_("Error - %s\n"), strerror(saved_errno));
 	exit(64);
@@ -59,22 +63,27 @@ static void exit_with_error(int saved_errno, int quiet)
 
 int main(int argc, char **argv)
 {
-	int enabled;
+	int i, enabled;
 	int quiet = 0;
+	int require_shared = 0;
 
 	setlocale(LC_MESSAGES, "");
 	bindtextdomain(PACKAGE, LOCALEDIR);
 	textdomain(PACKAGE);
 
-	if (argc > 2) {
+	if (argc > 3) {
 		printf(_("unknown or incompatible options\n"));
 		print_help(argv[0]);
-	} else if (argc == 2) {
-		if (strcmp(argv[1], "--quiet") == 0 ||
-		    strcmp(argv[1], "-q") == 0) {
+	}
+	for (i = 1; i < argc; i++) {
+		if (strcmp(argv[i], "--quiet") == 0 ||
+		    strcmp(argv[i], "-q") == 0) {
 			quiet = 1;
-		} else if (strcmp(argv[1], "--help") == 0 ||
-			   strcmp(argv[1], "-h") == 0) {
+		} else if (strcmp(argv[i], "--exclusive") == 0 ||
+		    strcmp(argv[i], "-x") == 0) {
+			require_shared = 1;
+		} else if (strcmp(argv[i], "--help") == 0 ||
+			   strcmp(argv[i], "-h") == 0) {
 			print_help(argv[0]);
 		} else {
 			printf(_("unknown option '%s'\n"), argv[1]);
@@ -83,9 +92,10 @@ int main(int argc, char **argv)
 	}
 
 	enabled = aa_is_enabled();
-	if (!enabled)
-		exit_with_error(errno, quiet);
-
+	if (!enabled) {
+		if (require_shared || errno != EBUSY)
+			exit_with_error(errno, quiet);
+	}
 	if (!quiet)
 		printf(_("Yes\n"));
 	exit(0);
