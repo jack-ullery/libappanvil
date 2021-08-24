0ee225b498a333b57cfbd5fe42daa58c673f52ba
Christian Boltz <apparmor@cboltz.de>
2021-08-24 22:48:19 +0200

    import hasher from apparmor.common
    ... instead of indirectly using it via apparmor.aa

--- utils/test/test-libapparmor-test_multi.py
+++ utils/test/test-libapparmor-test_multi.py
@@ -14,7 +14,7 @@ from common_test import AATest, setup_all_loops, setup_aa, read_file
 
 import os
 import sys
-from apparmor.common import open_file_read, split_name
+from apparmor.common import hasher, open_file_read, split_name
 
 import apparmor.aa
 from apparmor.logparser import ReadLog
@@ -225,7 +225,7 @@ def logfile_to_profile(logfile):
 
     # cleanup apparmor.aa storage
     apparmor.aa.log = dict()
-    apparmor.aa.aa = apparmor.aa.hasher()
+    apparmor.aa.aa = hasher()
 
     profile, hat = split_name(parsed_event['profile'])
 
