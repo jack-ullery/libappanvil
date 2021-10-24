1da0305c890db64f0ae2b024536e86b9d611ddee
Christian Boltz <apparmor@cboltz.de>
2021-10-24 15:29:25 +0200

    Add tests for sane_timestamp
    Ensure that pre-2000 and post-2050 dates get rejected, and something in
    between gets accepted.
    
    This also extends coverage to 100% - before, the post-2050 branch was
    not covered.

--- utils/test/test-notify.py
+++ utils/test/test-notify.py
@@ -13,7 +13,17 @@ import unittest
 from common_test import AATest, setup_all_loops
 
 from apparmor.common import AppArmorBug
-from apparmor.notify import get_last_login_timestamp
+from apparmor.notify import get_last_login_timestamp, sane_timestamp
+
+class TestSane_timestamp(AATest):
+    tests = [
+        (2524704400,    False), # Sun Jan  2 03:46:40 CET 2050
+        ( 944780400,    False), # Fri Dec 10 00:00:00 CET 1999
+        (1635026400,    True ), # Sun Oct 24 00:00:00 CEST 2021
+    ]
+
+    def _run_test(self, params, expected):
+        self.assertEqual(sane_timestamp(params), expected)
 
 class TestGet_last_login_timestamp(AATest):
     tests = [
