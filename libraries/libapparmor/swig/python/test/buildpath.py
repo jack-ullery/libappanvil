fda390983f77c2d218690522eba35aecf20aa332
David Runge <dave@sleepmap.de>
2022-08-02 14:17:27 +0200

    Fix setuptools version detection in buildpath.py
    libraries/libapparmor/swig/python/test/buildpath.py:
    The changes introduced in
    https://gitlab.com/apparmor/apparmor/-/commit/cc7f549665282c0a527d5424a6f9d726c50ddbb1
    targetted a wrong setuptools version (61.2).
    The change in build directory naming has been introduced with 62.1
    (https://github.com/pypa/setuptools/commit/1c23f5e1e4b18b50081cbabb2dea22bf345f5894).

--- libraries/libapparmor/swig/python/test/buildpath.py
+++ libraries/libapparmor/swig/python/test/buildpath.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-# the build path has changed in setuptools 61.2
+# the build path has changed in setuptools 62.1:
+# https://github.com/pypa/setuptools/commit/1c23f5e1e4b18b50081cbabb2dea22bf345f5894
 import sys
 import sysconfig
 import setuptools
-if tuple(map(int,setuptools.__version__.split("."))) >= (61, 2):
+
+
+if tuple(map(int, setuptools.__version__.split("."))) >= (62, 1):
     identifier = sys.implementation.cache_tag
 else:
     identifier = "%d.%d" % sys.version_info[:2]
