931a853a92c84c698dc1b9d77018968c766bc7ba
Jack Ullery <46848683+jack-ullery@users.noreply.github.com>
2023-01-24 18:44:48 -0500

    minor formatting

--- test/e2e/src/file_rules.cc
+++ test/e2e/src/file_rules.cc
@@ -8,31 +8,31 @@
 #include "apparmor_parser.hh"
 
 namespace FileRuleCheck {
-    std::list<AppArmor::Profile> getProfileList(std::string filename)
-    {
-        std::ifstream stream(filename);
-        AppArmor::Parser parser(stream);
-        return parser.getProfileList();
-    }
+  std::list<AppArmor::Profile> getProfileList(std::string filename)
+  {
+    std::ifstream stream(filename);
+    AppArmor::Parser parser(stream);
+    return parser.getProfileList();
+  }
+  
+  void check_file_rules_for_single_profile(std::string filename, std::list<AppArmor::FileRule> expected_file_rules, std::string profile_name = "/does/not/exist")
+  {
+    auto profile_list = getProfileList(filename);
+  
+    EXPECT_EQ(profile_list.size(), 1) << "There should only be one profile";
+  
+    auto first_profile = profile_list.begin();
+    EXPECT_EQ(first_profile->name(), profile_name);
 
-    void check_file_rules_for_single_profile(std::string filename, std::list<AppArmor::FileRule> expected_file_rules, std::string profile_name = "/does/not/exist")
-    {
-        auto profile_list = getProfileList(filename);
+    auto file_rules = first_profile->getFileRules();
+    ASSERT_EQ(file_rules, expected_file_rules);
+  }
+  
+  TEST(FileRuleCheck, abi_ok_1)
+  {
+    auto filename = PROFILE_SOURCE_DIR "/abi/ok_1.sd";
+    std::list<AppArmor::FileRule> expected_abstractions{};
 
-        EXPECT_EQ(profile_list.size(), 1) << "There should only be one profile";
-
-        auto first_profile = profile_list.begin();
-        EXPECT_EQ(first_profile->name(), profile_name);
-
-        auto file_rules = first_profile->getFileRules();
-        ASSERT_EQ(file_rules, expected_file_rules);
-    }
-
-    TEST(FileRuleCheck, abi_ok_1)
-    {
-        auto filename = PROFILE_SOURCE_DIR "/abi/ok_1.sd";
-        std::list<AppArmor::FileRule> expected_abstractions{};
-
-        check_file_rules_for_single_profile(filename, expected_abstractions);
-    }
+    check_file_rules_for_single_profile(filename, expected_abstractions);
+  }
 }
