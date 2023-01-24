c33570ab8d935200090af3d3fe21735acfa564fe
Jack Ullery <46848683+jack-ullery@users.noreply.github.com>
2023-01-24 17:36:26 -0500

    move AbstractionCheck tests to own namespace

--- test/e2e/src/abstractions.cc
+++ test/e2e/src/abstractions.cc
@@ -7,125 +7,127 @@
 
 #include "apparmor_parser.hh"
 
-std::list<AppArmor::Profile> getProfileList(std::string filename)
-{
-	std::ifstream stream(filename);
-	AppArmor::Parser parser(stream);
-	return parser.getProfileList();
-}
+namespace AbstractionCheck {
+    std::list<AppArmor::Profile> getProfileList(std::string filename)
+    {
+        std::ifstream stream(filename);
+        AppArmor::Parser parser(stream);
+        return parser.getProfileList();
+    }
 
-void check_abstractions_for_single_profile(std::string filename, std::unordered_set<std::string> expected_abstractions, std::string profile_name = "/does/not/exist")
-{
-    auto profile_list = getProfileList(filename);
+    void check_abstractions_for_single_profile(std::string filename, std::unordered_set<std::string> expected_abstractions, std::string profile_name = "/does/not/exist")
+    {
+        auto profile_list = getProfileList(filename);
 
-    EXPECT_EQ(profile_list.size(), 1) << "There should only be one profile";
+        EXPECT_EQ(profile_list.size(), 1) << "There should only be one profile";
 
-    auto first_profile = profile_list.begin();
-    EXPECT_EQ(first_profile->name(), profile_name);
+        auto first_profile = profile_list.begin();
+        EXPECT_EQ(first_profile->name(), profile_name);
 
-    auto abstractions = first_profile->getAbstractions();
-    EXPECT_EQ(abstractions, expected_abstractions);
-}
+        auto abstractions = first_profile->getAbstractions();
+        EXPECT_EQ(abstractions, expected_abstractions);
+    }
 
-TEST(AbstractionCheck, abi_ok_1)
-{
-    auto filename = PROFILE_SOURCE_DIR "/abi/ok_1.sd";
-    std::unordered_set<std::string> expected_abstractions{};
+    TEST(AbstractionCheck, abi_ok_1)
+    {
+        auto filename = PROFILE_SOURCE_DIR "/abi/ok_1.sd";
+        std::unordered_set<std::string> expected_abstractions{};
 
-    check_abstractions_for_single_profile(filename, expected_abstractions);
-}
+        check_abstractions_for_single_profile(filename, expected_abstractions);
+    }
 
-TEST(AbstractionCheck, bare_include_tests_ok_1)
-{
-    auto filename = PROFILE_SOURCE_DIR "/bare_include_tests/ok_1.sd";
-    std::unordered_set<std::string> expected_abstractions{
-        "includes/base",
-        "include_tests/includes_okay_helper.include"
-    };
+    TEST(AbstractionCheck, bare_include_tests_ok_1)
+    {
+        auto filename = PROFILE_SOURCE_DIR "/bare_include_tests/ok_1.sd";
+        std::unordered_set<std::string> expected_abstractions{
+            "includes/base",
+            "include_tests/includes_okay_helper.include"
+        };
 
-    check_abstractions_for_single_profile(filename, expected_abstractions);
-}
+        check_abstractions_for_single_profile(filename, expected_abstractions);
+    }
 
-TEST(AbstractionCheck, bare_include_tests_ok_2)
-{
-    auto filename = PROFILE_SOURCE_DIR "/bare_include_tests/ok_2.sd";
-    std::unordered_set<std::string> expected_abstractions{
-        "includes/base",
-        "include_tests/includes_okay_helper.include"
-    };
+    TEST(AbstractionCheck, bare_include_tests_ok_2)
+    {
+        auto filename = PROFILE_SOURCE_DIR "/bare_include_tests/ok_2.sd";
+        std::unordered_set<std::string> expected_abstractions{
+            "includes/base",
+            "include_tests/includes_okay_helper.include"
+        };
 
-    check_abstractions_for_single_profile(filename, expected_abstractions);
-}
+        check_abstractions_for_single_profile(filename, expected_abstractions);
+    }
 
-TEST(AbstractionCheck, bare_include_tests_ok_3)
-{
-    auto filename = PROFILE_SOURCE_DIR "/bare_include_tests/ok_3.sd";
-    std::unordered_set<std::string> expected_abstractions{
-        "includes/base",
-        "includes/"
-    };
+    TEST(AbstractionCheck, bare_include_tests_ok_3)
+    {
+        auto filename = PROFILE_SOURCE_DIR "/bare_include_tests/ok_3.sd";
+        std::unordered_set<std::string> expected_abstractions{
+            "includes/base",
+            "includes/"
+        };
 
-    check_abstractions_for_single_profile(filename, expected_abstractions);
-}
+        check_abstractions_for_single_profile(filename, expected_abstractions);
+    }
 
-TEST(AbstractionCheck, bare_include_tests_ok_11)
-{
-    auto filename = PROFILE_SOURCE_DIR "/bare_include_tests/ok_11.sd";
-    std::unordered_set<std::string> expected_abstractions{
-        "simple_tests/include_tests/includes_okay_helper.include",
-    };
+    TEST(AbstractionCheck, bare_include_tests_ok_11)
+    {
+        auto filename = PROFILE_SOURCE_DIR "/bare_include_tests/ok_11.sd";
+        std::unordered_set<std::string> expected_abstractions{
+            "simple_tests/include_tests/includes_okay_helper.include",
+        };
 
-    check_abstractions_for_single_profile(filename, expected_abstractions);
-}
+        check_abstractions_for_single_profile(filename, expected_abstractions);
+    }
 
-TEST(AbstractionCheck, bare_include_tests_ok_12)
-{
-    auto filename = PROFILE_SOURCE_DIR "/bare_include_tests/ok_12.sd";
-    std::unordered_set<std::string> expected_abstractions{
-        "../tst/simple_tests/include_tests/includes_okay_helper.include",
-    };
+    TEST(AbstractionCheck, bare_include_tests_ok_12)
+    {
+        auto filename = PROFILE_SOURCE_DIR "/bare_include_tests/ok_12.sd";
+        std::unordered_set<std::string> expected_abstractions{
+            "../tst/simple_tests/include_tests/includes_okay_helper.include",
+        };
 
-    check_abstractions_for_single_profile(filename, expected_abstractions);
-}
+        check_abstractions_for_single_profile(filename, expected_abstractions);
+    }
 
-TEST(AbstractionCheck, bare_include_tests_ok_13)
-{
-    auto filename = PROFILE_SOURCE_DIR "/bare_include_tests/ok_13.sd";
-    std::unordered_set<std::string> expected_abstractions{
-        "./simple_tests/include_tests/includes_okay_helper.include",
-    };
+    TEST(AbstractionCheck, bare_include_tests_ok_13)
+    {
+        auto filename = PROFILE_SOURCE_DIR "/bare_include_tests/ok_13.sd";
+        std::unordered_set<std::string> expected_abstractions{
+            "./simple_tests/include_tests/includes_okay_helper.include",
+        };
 
-    check_abstractions_for_single_profile(filename, expected_abstractions);
-}
+        check_abstractions_for_single_profile(filename, expected_abstractions);
+    }
 
-TEST(AbstractionCheck, bare_include_tests_ok_14)
-{
-    auto filename = PROFILE_SOURCE_DIR "/bare_include_tests/ok_14.sd";
-    std::unordered_set<std::string> expected_abstractions{
-        "includes/base",
-        "../tst/simple_tests/include_tests/includes_okay_helper.include",
-    };
+    TEST(AbstractionCheck, bare_include_tests_ok_14)
+    {
+        auto filename = PROFILE_SOURCE_DIR "/bare_include_tests/ok_14.sd";
+        std::unordered_set<std::string> expected_abstractions{
+            "includes/base",
+            "../tst/simple_tests/include_tests/includes_okay_helper.include",
+        };
 
-    check_abstractions_for_single_profile(filename, expected_abstractions);
-}
+        check_abstractions_for_single_profile(filename, expected_abstractions);
+    }
 
-TEST(AbstractionCheck, bare_include_tests_ok_15)
-{
-    auto filename = PROFILE_SOURCE_DIR "/bare_include_tests/ok_15.sd";
-    std::unordered_set<std::string> expected_abstractions{
-        "includes/base",
-        "simple_tests/includes/",
-    };
+    TEST(AbstractionCheck, bare_include_tests_ok_15)
+    {
+        auto filename = PROFILE_SOURCE_DIR "/bare_include_tests/ok_15.sd";
+        std::unordered_set<std::string> expected_abstractions{
+            "includes/base",
+            "simple_tests/includes/",
+        };
 
-    check_abstractions_for_single_profile(filename, expected_abstractions);
-}
+        check_abstractions_for_single_profile(filename, expected_abstractions);
+    }
 
-TEST(AbstractionCheck, rewrite_alias_good_2)
-{
-    auto filename = PROFILE_SOURCE_DIR "/rewrite/alias_good_2.sd";
-    std::unordered_set<std::string> expected_abstractions{
-        "includes/base",
-    };
+    TEST(AbstractionCheck, rewrite_alias_good_2)
+    {
+        auto filename = PROFILE_SOURCE_DIR "/rewrite/alias_good_2.sd";
+        std::unordered_set<std::string> expected_abstractions{
+            "includes/base",
+        };
 
-    check_abstractions_for_single_profile(filename, expected_abstractions, "/bin/foo");
+        check_abstractions_for_single_profile(filename, expected_abstractions, "/bin/foo");
+    }
 }
