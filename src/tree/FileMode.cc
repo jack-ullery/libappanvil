331f3186b3d0d3dc13ceb8071061903a5690f26d
Jack Ullery <46848683+jack-ullery@users.noreply.github.com>
2023-06-08 22:18:50 -0400

    change how FileMode parses execute permissions

--- src/tree/FileMode.cc
+++ src/tree/FileMode.cc
@@ -3,11 +3,13 @@
 
 #include <cctype>
 #include <format>
+#include <iterator>
 #include <stdexcept>
+#include <sstream>
 
 AppArmor::Tree::FileMode::FileMode(const std::string &mode)
 {
-  bool expect_execute = false;
+  std::stringstream potential_execute_mode;
   for(const char &ch : mode) {
     auto lower_ch = std::tolower(ch);
     switch(lower_ch) {
@@ -50,22 +52,17 @@ AppArmor::Tree::FileMode::FileMode(const std::string &mode)
       case COD_PROFILE_CHAR:
       case COD_UNSAFE_LOCAL_CHAR:
       case COD_LOCAL_CHAR:
-        execute_mode = std::format("{}x", ch);
-        expect_execute = true;
+        potential_execute_mode << ch;
         continue;
 
       case COD_EXEC_CHAR:
-        if(!expect_execute) {
-          throw std::runtime_error("Character 'x' encountered, but the type of execute mode was not specified (e.g. 'ix', 'px', 'cx', etc).\n"
-                                   "Please reference \"Access Modes\" from `man apparmor.d`.");
-        }
+        potential_execute_mode << ch;
+        execute_mode = potential_execute_mode.str();
         break;
 
       default:
         throw std::runtime_error(std::format("Encountered unexpected character when parsing file mode: {}", ch));
     }
-
-    expect_execute = false;
   }
 }
 
