22de7f54d3665902486a005761c3472d4f4769ba
Jack Ullery <46848683+jack-ullery@users.noreply.github.com>
2023-09-27 16:46:54 -0400

    create test for when updateFromString fails

--- test/e2e/src/parser.cc
+++ test/e2e/src/parser.cc
@@ -16,18 +16,18 @@ TEST(ParserCheck, update_from_string)
   ASSERT_NE(parser_1.getProfileList(), parser_2.getProfileList());
 }
 
-  // TEST(ParserCheck, update_from_string_fail)
-  // {
-  //   auto profile = PROFILE_SOURCE_DIR "/file/ok_1.sd";
-  //   auto new_profile_data = "/usr/bin/foo { /usr/bin/foo rwix }";
+TEST(ParserCheck, update_from_string_fail)
+{
+  auto profile = PROFILE_SOURCE_DIR "/file/ok_1.sd";
+  auto new_profile_data = "/usr/bin/foo { /usr/bin/foo rwix }";
 
-  //   AppArmor::Parser parser_1(profile);
-  //   AppArmor::Parser parser_2(profile);
+  AppArmor::Parser parser_1(profile);
+  AppArmor::Parser parser_2(profile);
 
-  //   ASSERT_EQ(parser_1.getProfileList(), parser_2.getProfileList());
-  //   ASSERT_ANY_THROW(parser_1.updateFromString(new_profile_data));
-  //   ASSERT_EQ(parser_1.getProfileList(), parser_2.getProfileList());
-  // }
+  ASSERT_EQ(parser_1.getProfileList(), parser_2.getProfileList());
+  ASSERT_NO_FATAL_FAILURE(ASSERT_ANY_THROW(parser_1.updateFromString(new_profile_data)));
+  ASSERT_EQ(parser_1.getProfileList(), parser_2.getProfileList());
+}
 
 // Broken profiles should throw an exception, without a greater segfault
 TEST_P(ParserCheck, fail_gracefully)
